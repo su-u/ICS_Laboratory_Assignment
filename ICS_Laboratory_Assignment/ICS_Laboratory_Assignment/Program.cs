1588ab7ebef4999e569643ef514ed9b72c0f8e19
すー <aaaiiiuuu35@gmail.com>
2019-06-12 23:04:18 +0900

    init project

---
+++ ICS_Laboratory_Assignment/ICS_Laboratory_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICS_Laboratory_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
