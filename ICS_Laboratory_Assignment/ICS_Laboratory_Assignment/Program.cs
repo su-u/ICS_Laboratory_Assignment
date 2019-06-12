b1d4069cc18e6f08eece494af581be99f11f7337
すー <aaaiiiuuu35@gmail.com>
2019-06-13 00:13:45 +0900

    Update Program.cs

--- ICS_Laboratory_Assignment/ICS_Laboratory_Assignment/Program.cs
+++ ICS_Laboratory_Assignment/ICS_Laboratory_Assignment/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace ICS_Laboratory_Assignment
 {
+    class Student
+    {
+        public int Number { get; private set; }
+        public double Gpa { get; set; }
+        public readonly List<Tuple<int,int>> Satisfaction;
+
+
+        public Student(int num, double gpa, List<int> satisfaction)
+        {
+            this.Number = num;
+            this.Gpa = gpa;
+            for (int i = 1; i < satisfaction.Count; i++)
+            {
+                this.Satisfaction.Add(Tuple.Create(i, satisfaction[i - 1]));
+            }
+            this.Satisfaction = this.Satisfaction.OrderBy(x => x.Item2).ToList();
+        }
+    }
+
+    class Laboratory
+    {
+        public string Name { get; private set; }
+        public int Number { get; private set; }
+        private readonly int MAX;
+        private List<Student> Students;
+
+        public Laboratory(int num, string name, int max)
+        {
+            this.Name = name;
+            this.Number = num;
+            this.MAX = max;
+            Students = new List<Student>();
+        }
+
+        public int Size => this.Students.Count;
+
+        public Student this[int i] => this.Students[i];
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> unassigned = new List<Student>();
+
+            List<Laboratory> laboratory = new List<Laboratory>
+            {
+                new Laboratory(1, "清水研", 14),
+                new Laboratory(2, "水津研", 14),
+                new Laboratory(3, "菅原研", 14),
+                new Laboratory(4, "中静研", 14),
+                new Laboratory(5, "枚田研", 14),
+                new Laboratory(6, "中林研", 14),
+                new Laboratory(7, "藤原研", 14),
+                new Laboratory(8, "木下研", 8),
+                new Laboratory(9, "糸井研", 8),
+                new Laboratory(10, "長研", 14),
+            };
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()?.Split().ToList();
+                unassigned.Add(new Student(int.Parse(line?[0]), double.Parse(line[1]), line.Skip(2).Select(int.Parse).ToList()));
+            }
+
+            unassigned = unassigned.OrderBy(x => x.Gpa).ToList();
+
+            foreach (var student in unassigned)
+            {
+                Console.WriteLine(student.Satisfaction.GroupBy(x => x).Where(x => x.Key.Item1 == student.Satisfaction.Max().Item2));
+            }
+
+
         }
     }
 }
